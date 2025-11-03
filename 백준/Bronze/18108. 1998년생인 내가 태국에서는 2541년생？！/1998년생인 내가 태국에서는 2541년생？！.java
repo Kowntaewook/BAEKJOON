8adb29bf93b5095dde35651737fd1aeecd67f824
kwonta3wook <152182535+Kowntaewook@users.noreply.github.com>
2025-11-03 13:46:58 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1 = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(num1 - 543);
+	}
+
+}
